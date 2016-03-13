e359ddab9cd0d5f17e38888e318f7ed39b6af06d
yh1126 <cake_sk_yh@icloud.com>
2016-03-13 22:00:22 +0900

    class化した

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,39 @@
 #mod演算子を用いたfizzbuzz
-v = 1
-name = "yamashita!"
-msg = "hello"
-p msg + name 
+class Fizzbuzz
+#whileは終わりが見えないときだからforで書くべき
+#コード読むの人のことを考えるとelseifはあまり書かないほうがいい
+#初期条件と比較しながら見るため
 
-while v < 31
-  if v%15 == 0
-    p "fizzbuzz"
-  elsif v%5 == 0
-    p "buzz"
-  elsif v%3 == 0
-    p "fizz"
-  else
-    p v
+  FIZZ = 3
+  BUZZ = 5
+
+  def exec(max)
+    for i in 1..max do
+      puts judge(i)
+    end
   end
-  v += 1
+
+  private
+  def judge(n)
+#===で型まで同じかどうか調べる
+#== 抽象等価演算
+#===厳密等価演算
+  if n % FIZZ === 0 && n % BUZZ === 0
+    return "FIZZBUZZ"
+  end
+
+  if n % FIZZ === 0
+    return "FIZZ"
+  end
+
+  if n % BUZZ === 0
+    return "BUZZ"
+  end
+
+  return n
+
+  end
 end
+
+fb = Fizzbuzz.new
+fb.exec(30)
--- fizzbuzz2.rb
+++ fizzbuzz2.rb
@@ -1,19 +1,35 @@
 #mod演算子を使わないfizzbuzz
-num = [*(1..31)]
-i , j = 1 , 1
+class Fizzbuzz
+  FIZZ = 3
+  BUZZ = 5
+  @@i , @@j = 1 , 1
+  def exec(n)
+    for x in 1..n do
+      p judge(x)
+    end
+  end
 
-for n in num do
-  if (n/3 == i) && (n/5 == j)
-    i += 1
-    j += 1
-    p "fizzbuzz"
-  elsif n/5 == j
-    j += 1
-    p "buzz"
-  elsif n/3 == i
-    i += 1
-    p "fizz"
-  else
-    p n
+  private
+  def judge(m)
+    if m / FIZZ === @@i && m / BUZZ === @@j
+      @@i += 1
+      @@j += 1
+      return 'FIZZBUZZ'
+    end
+
+    if m / FIZZ === @@i
+      @@i += 1
+      return 'FIZZ'
+    end
+
+    if m / BUZZ === @@j
+      @@j += 1
+      return 'BUZZ'
+    end
+
+    return m
   end
 end
+
+fb = Fizzbuzz.new
+fb.exec(30)
